47757030caf888a3ac136254cb18b0ad20059dce
averygreen <24798374+AveryGreen@users.noreply.github.com>
2017-12-24 09:55:45 -0600

    Added waterfox cleaner and rsync logfiles

--- .bin/backup.sh
+++ .bin/backup.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
-rsync -avr --delete /run/media/mike/External /backups
-rsync -avr --delete --exclude=/home/mike/VirtualBox\ VMs/ /home /backups
+rsync -avr --delete --log-file=/home/mike/rsynclogs/backupHome-$(date +%F_%R).log /run/media/mike/External /backups
+rsync -avr --delete --exclude=/home/mike/VirtualBox\ VMs/ --log-file=/home/mike/rsynclogs/backupExternal-$(date +%F_%R).log /home /backups
